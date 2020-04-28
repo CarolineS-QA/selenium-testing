3d4b3aa18e2ae9e249cb31a7ec6dde598b4831fd
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-04-28 16:18:45 +0100

    added example selenium test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,8 +2,14 @@ package org.example;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.Thread.*;
+import static org.junit.Assert.assertTrue;
 
 public class AppTest
 {
@@ -12,6 +18,24 @@ public class AppTest
     @Before
     public void setUp(){
         driver = new ChromeDriver();
+    }
+
+    @Test
+    public void seleniumExampleTest() throws InterruptedException {
+        driver.manage().window().maximize();
+        sleep(1000);
+        driver.get("http://www.google.co.uk");
+        sleep(1000);
+        WebElement googleSearchBar = driver.findElement(By.name("q"));
+        googleSearchBar.sendKeys("funny dog pics");
+        sleep(1000);
+        googleSearchBar.submit();
+        sleep(1000);
+        WebElement linkToPictures =  driver.findElement(By.partialLinkText("Images for funny dog"));
+        linkToPictures.click();
+        sleep(1000);
+        WebElement imagesLink = driver.findElement(By.className("rQEFy"));
+        assertTrue(imagesLink.isDisplayed());
     }
 
     @After
