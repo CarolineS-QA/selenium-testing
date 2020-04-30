600c63b94efc96514ab7e119d4a6603a6a468333
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-04-30 15:08:04 +0100

    added ExtentReportExerciseTest with a Test that navigates to the stock market website

---
+++ src/test/java/org/example/ExtentReportExerciseTest.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+import java.io.File;
+
+public class ExtentReportExerciseTest {
+    WebDriver driver;
+    ExtentReports report;
+    ExtentTest test;
+
+    @BeforeTest
+    public void startReport(){
+        report = new ExtentReports(
+                System.getProperty("user.dir") + "/test-output/Report.html",
+                true
+        );
+        report
+                .addSystemInfo("Host Name", "QA")
+                .addSystemInfo("Tester", "Caroline");
+        report.loadConfig(new File(System.getProperty("user.dir") + "\\extent-report.xml"));
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void latestStockPriceIndex(){
+        test = report.startTest("Starting latestStockPriceIndex example test");
+        driver.manage().window().maximize();
+        test.log(LogStatus.INFO, "Started chrome browser and made it fullscreen");
+        driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100");
+        test.log(LogStatus.INFO, "Navigating to the stock market website");
+    }
+
+    @AfterMethod
+    public void getResult(ITestResult result){
+        driver.close();
+        if(result.getStatus() == ITestResult.FAILURE){
+            test.log(LogStatus.FAIL, "Test has failed " + result.getName());
+            test.log(LogStatus.FAIL, "Test has failed " + result.getThrowable());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(LogStatus.PASS, "Test has passed " + result.getName());
+        }
+        report.endTest(test);
+    }
+
+    @AfterTest
+    public void endReport(){
+        report.flush();
+        report.close();
+    }
+}
